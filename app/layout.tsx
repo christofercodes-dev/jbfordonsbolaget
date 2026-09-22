d27610ec0d69e166dc1a355231cbe962aaa60513
christofercodes-dev <christofer.codes@gmail.com>
2026-09-22 10:22:31 +0200

    Remove SmoothScroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import SmoothScroll from '@/components/SmoothScroll'; // Importera här
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
