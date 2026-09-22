65f9286f95b5525569700fdbc648c403dfad5827
christofercodes-dev <christofer.codes@gmail.com>
2026-09-22 11:26:09 +0200

    Scroll fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
 import Hero from "@/components/Hero";
-import Process from "@/components/Process";
-import Stats from "@/components/Stats";
-import Atmosphere from "@/components/Atmosphere";
-import Features from "@/components/Features";
-import Valuation from "@/components/Valuation";
-import TrustBar from "@/components/TrustBar";
 import SellCar from "@/components/SellCar";
-import About from "@/components/About";
 import Info from "@/components/Info";
 import InfoCards from "@/components/InfoCards";
 import BrandsBanner from "@/components/BrandsBanner";
@@ -15,13 +8,10 @@ export default function Home() {
   return (
     <main>
       <Hero />
-      <Info/>
-{/*       <TrustBar/>
- */}      <SellCar/>
- <BrandsBanner/>
- <InfoCards/>
-      {/* <Process /> */}
-   
+      <Info />
+      <SellCar />
+      <BrandsBanner />
+      <InfoCards />
     </main>
   );
 }
